7c0d2346f1ce272623a81b191614d6eb839d131b
Xuezhou Dai <daidr@daidr.me>
2024-02-04 09:22:03 +0000

    feat: postbuild script support

--- packages/debug/vite.config.ts
+++ packages/debug/vite.config.ts
@@ -1,10 +1,18 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import dts from 'vite-plugin-dts'
+import { visualizer } from 'rollup-plugin-visualizer'
 
 export default defineConfig({
-  plugins: [vue(), dts({ include: '../core' })],
+  plugins: [vue(), dts({ include: '../core' }), visualizer({
+    gzipSize: true,
+    brotliSize: true,
+    sourcemap: true,
+    filename: '../../stats.html',
+  })],
   build: {
+    emptyOutDir: true,
+    sourcemap: true,
     outDir: '../../dist',
     lib: {
       entry: '../core/index.ts',
---
+++ scripts/postbuild.ts
@@ -0,0 +1,49 @@
+/* eslint-disable no-console */
+import fs from 'node:fs'
+import packageJson from '../package.json'
+import corePackageJson from '../packages/core/package.json'
+
+async function run() {
+  console.log('Running postbuild script')
+  // check if dist folder exists
+  if (!fs.existsSync('dist')) {
+    console.log('dist folder does not exist')
+  }
+
+  // copy LICENSE and README.md to dist folder
+  console.log('Copying LICENSE')
+  fs.copyFileSync('LICENSE', 'dist/LICENSE')
+  console.log('Copying README.md')
+  fs.copyFileSync('README.md', 'dist/README.md')
+
+  // generate package.json
+
+  if (!packageJson || !packageJson.version) {
+    console.error('package.json or version is not defined')
+    return
+  }
+
+  const finalPackageJson = {
+    name: '@vuemark/core',
+    version: packageJson.version,
+    type: 'module',
+    main: 'vuemark.umd.cjs',
+    module: 'vuemark.js',
+    types: 'vuemark.d.ts',
+    exports: {
+      '.': {
+        import: './vuemark.js',
+        require: './vuemark.umd.cjs',
+        types: './vuemark.d.ts',
+      },
+    },
+    dependencies: corePackageJson.devDependencies,
+  }
+
+  console.log('Writing package.json')
+  fs.writeFileSync('dist/package.json', JSON.stringify(finalPackageJson, null, 2))
+
+  console.log('Postbuild script completed')
+}
+
+run()
